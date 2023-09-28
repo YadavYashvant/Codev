d6eeba2678567482cfbb8b471e49caa603db55f5
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-28 20:54:32 +0530

    making searchbar

--- app/src/main/java/com/example/codev/MainActivity.kt
+++ app/src/main/java/com/example/codev/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.example.codev
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.codev.ui.theme.CodevTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +44,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        /*topBar = {
+                            TopAppBar(
+                                title = { Text(text = "Codev") },
+                                navigationIcon = { Icon(Icons.Filled.Menu, contentDescription = null) }
+                                actions = { *//* TODO *//* }
+                            )
+                        }*/
+
+                        topBar = {
+                            var value by remember { mutableStateOf("") }
+                            val onValueChange: (String) -> Unit = { value = it }
+                            TextField(
+                                value = value,
+                                onValueChange = onValueChange,
+                                shape = MaterialTheme.shapes.extraLarge,
+                                placeholder = { Text(text = "Search Projects") },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp),
+                                colors = TextFieldDefaults.textFieldColors(
+                                    unfocusedIndicatorColor = Color.Transparent,
+                                    focusedIndicatorColor = Color.Transparent
+                                ),
+                                leadingIcon = { Icon(imageVector = Icons.Outlined.Search, contentDescription = null, Modifier
+                                    .scale(1.3F)
+                                    .padding(10.dp)) },
+
+                            )
+                        }
+                    ) {
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CodevTheme {
-        Greeting("Android")
     }
 }
