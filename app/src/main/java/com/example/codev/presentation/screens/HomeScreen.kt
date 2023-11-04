5cc9e77ddb905b302eaf3ef58edc794d445cee37
yashvant <yashvantyadav855@gmail.com>
2023-11-05 01:52:49 +0530

    changing home screen UI

--- app/src/main/java/com/example/codev/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/codev/presentation/screens/HomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -67,180 +68,174 @@ fun HomeScreen(
             /*.padding(bottom = 66.dp, top = 66.dp)*/
     ) {
 
-        val scrollState = rememberScrollState()
+        /*val scrollState = rememberScrollState()*/
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(scrollState)
+                /*.verticalScroll(scrollState)*/
                 .padding(vertical = 16.dp)
         ) {
-
-
-
             var value by remember { mutableStateOf("") }
             val onValueChange: (String) -> Unit = { value = it }
 
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(horizontal = 32.dp)
-                    .padding(bottom = 48.dp)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 50.dp)
-                    ,
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    if(userData?.profilePicture != null) {
-                        AsyncImage(
-                            model = userData?.profilePicture, contentDescription = null,
+            LazyColumn{
+                item {
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .padding(horizontal = 32.dp)
+                            .padding(bottom = 48.dp)
+                    ) {
+                        Column(
                             modifier = Modifier
-                                .size(150.dp)
-                                .clip(CircleShape),
-                            contentScale = ContentScale.Crop
-                        )
+                                .fillMaxWidth()
+                                .padding(vertical = 50.dp)
+                            ,
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            if(userData?.profilePicture != null) {
+                                AsyncImage(
+                                    model = userData?.profilePicture, contentDescription = null,
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .clip(CircleShape),
+                                    contentScale = ContentScale.Crop
+                                )
 
-                        Spacer(modifier = Modifier.height(20.dp))
-                    }
+                                Spacer(modifier = Modifier.height(20.dp))
+                            }
 
-                    if(userData?.username != null) {
-                        Text(
-                            text = userData.username,
-                            textAlign = TextAlign.Center,
-                            fontSize = 25.sp,
-                            fontWeight = FontWeight.SemiBold,
-                            fontFamily = fontfamily
-                        )
-                        Spacer(modifier = Modifier.height(20.dp))
-                    }
-                    /*OutlinedButton(*//*onClick = onSignOut*//*) {
+                            if(userData?.username != null) {
+                                Text(
+                                    text = userData.username,
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.SemiBold,
+                                    fontFamily = fontfamily
+                                )
+                                Spacer(modifier = Modifier.height(20.dp))
+                            }
+                            /*OutlinedButton(*//*onClick = onSignOut*//*) {
                         Text(text = "Sign out")
                     }*/
-                }
-            }
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                ,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                TextField(
-                    value = value,
-                    onValueChange = onValueChange,
-                    shape = MaterialTheme.shapes.extraLarge,
-                    placeholder = {
-                        Text(
-                            text = "Search Projects",
-                            Modifier.align(Alignment.CenterVertically),
-                            fontFamily = com.example.codev.spacefamily,
-                            textAlign = TextAlign.Center,
-                        )
-                    },
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .fillMaxWidth()
-                    ,
-                    colors = TextFieldDefaults.textFieldColors(
-                        unfocusedIndicatorColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent
-                    ),
-                    leadingIcon = {
-                        Icon(
-                            imageVector = Icons.Outlined.Search, contentDescription = null,
-                            Modifier
-                                .scale(1.3F)
-                                .padding(10.dp)
-                        )
-                    },
-                )
-            }
-
-            OutlinedCard(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp)
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(20.dp)
-                ) {
-                    Text(
-                        fontFamily = spacefamily,
-                        text = "Codev", fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .align(Alignment.CenterHorizontally)
-                    )
-                    Text(
-
-                        text = "A collaborative space for developers " +
-                                "to find and list their undergoing projects " +
-                                "and ideas to be worked on and seek for team " +
-                                "members with requirements.",
-                        fontFamily = spacefamily,
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Normal,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .align(Alignment.CenterHorizontally)
-
-                    )
-                    Image(
-                        painter = painterResource(id = R.drawable.concept),
-                        contentDescription = "project image",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 16.dp)
-                            .align(Alignment.CenterHorizontally)
-                    )
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(10.dp)
-                            .align(Alignment.Start)
-                            .clip(shape = MaterialTheme.shapes.extraLarge)
-                            .background(color = Color.Cyan)
-                        /*.background(color = Color.LightGray)*/,
-                        horizontalArrangement = Arrangement.SpaceBetween
-
-                    ) {
-                        Button(
-                            onClick = { /*TODO*/ },
-                            modifier = Modifier
-                                .padding(vertical = 12.dp, horizontal = 16.dp)
-                        ) {
-                            Text(
-                                text = "View Project",
-                                fontFamily = spacefamily,
-                                fontWeight = FontWeight.Bold
-                            )
                         }
+                    }
 
-                        OutlinedButton(
-                            onClick = { /*TODO*/ },
-                            modifier = Modifier
-                                .padding(vertical = 12.dp, horizontal = 16.dp)
-                        ) {
+                    TextField(
+                        value = value,
+                        onValueChange = onValueChange,
+                        shape = MaterialTheme.shapes.extraLarge,
+                        placeholder = {
+                            Text(
+                                text = "Search Projects",
+                                fontFamily = com.example.codev.spacefamily,
+                                textAlign = TextAlign.Center,
+                            )
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                        ,
+                        colors = TextFieldDefaults.textFieldColors(
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent
+                        ),
+                        leadingIcon = {
                             Icon(
-                                imageVector = Icons.Filled.Info,
-                                contentDescription = "info",
-                                Modifier.padding(end = 5.dp)
+                                imageVector = Icons.Outlined.Search, contentDescription = null,
+                                Modifier
+                                    .scale(1.3F)
+                                    .padding(10.dp)
+                            )
+                        },
+                    )
+                }
+                items(10){
+                    OutlinedCard(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(20.dp)
+                        ) {
+                            Text(
+                                fontFamily = spacefamily,
+                                text = "Codev", fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally)
                             )
                             Text(
-                                text = "Inquire",
-                                fontFamily = spacefamily, /*fontWeight = FontWeight.Bold*/
+
+                                text = "A collaborative space for developers " +
+                                        "to find and list their undergoing projects " +
+                                        "and ideas to be worked on and seek for team " +
+                                        "members with requirements.",
+                                fontFamily = spacefamily,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Normal,
+                                textAlign = TextAlign.Center,
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally)
+
                             )
+                            Image(
+                                painter = painterResource(id = R.drawable.concept),
+                                contentDescription = "project image",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 16.dp)
+                                    .align(Alignment.CenterHorizontally)
+                            )
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(10.dp)
+                                    .align(Alignment.Start)
+                                    .clip(shape = MaterialTheme.shapes.extraLarge)
+                                    .background(color = Color.Cyan)
+                                /*.background(color = Color.LightGray)*/,
+                                horizontalArrangement = Arrangement.SpaceBetween
+
+                            ) {
+                                Button(
+                                    onClick = { /*TODO*/ },
+                                    modifier = Modifier
+                                        .padding(vertical = 12.dp, horizontal = 16.dp)
+                                ) {
+                                    Text(
+                                        text = "View Project",
+                                        fontFamily = spacefamily,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                }
+
+                                OutlinedButton(
+                                    onClick = { /*TODO*/ },
+                                    modifier = Modifier
+                                        .padding(vertical = 12.dp, horizontal = 16.dp)
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Info,
+                                        contentDescription = "info",
+                                        Modifier.padding(end = 5.dp)
+                                    )
+                                    Text(
+                                        text = "Inquire",
+                                        fontFamily = spacefamily, /*fontWeight = FontWeight.Bold*/
+                                    )
+                                }
+                            }
                         }
                     }
                 }
