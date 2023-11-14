3b57fdeb56aa221d97c4c7e2a16692beb5e0aa77
yashvant <yashvantyadav855@gmail.com>
2023-11-14 22:35:09 +0530

    doing some practice for interoperablity

---
+++ app/src/main/java/com/example/codev/myjavafile.java
@@ -0,0 +1,11 @@
+package com.example.codev;
+
+public class myjavafile {
+    public static void main(String[] args) {
+
+    }
+        public static String Hello(){
+            return "Hello, my name is Yashvant Yadav";
+        }
+
+}
---
+++ app/src/main/java/com/example/codev/utils/demokt.kt
@@ -0,0 +1,8 @@
+package com.example.codev.utils
+
+import com.example.codev.myjavafile
+
+fun main(args: Array<String>) {
+    var greeting: String = myjavafile.Hello()
+    println("Printing greeting from kotlin file: " + greeting)
+}
