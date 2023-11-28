b6eb792e379ab4772aecb4ed23c66f67e200061c
yashvant <yashvantyadav855@gmail.com>
2023-11-28 13:54:31 +0530

    implementing DI

---
+++ app/src/main/java/com/example/codev/firestore_feature/PostsRepository.kt
@@ -0,0 +1,29 @@
+package com.example.codev.firestore_feature
+
+import com.example.codev.data.DataOrException
+import com.example.codev.firestore_feature.model.Post
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class PostsRepository @Inject constructor(
+    private val queryPostsByName: Query
+) {
+
+    suspend fun getPostsFronFirestore(): DataOrException<List<Post>, Exception> {
+        val dataOrException = DataOrException<List<Post>, Exception>()
+
+        try {
+            dataOrException.data = queryPostsByName.get().await().map { document->
+                document.toObject(Post::class.java)
+            }
+        }catch (e: FirebaseFirestoreException) {
+            dataOrException.e = e
+        }
+        return dataOrException
+    }
+
+
+}
