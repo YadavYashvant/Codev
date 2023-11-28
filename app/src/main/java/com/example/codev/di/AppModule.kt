84bcdef2ba0f3ed1d25ce716d18c59719dede4ba
yashvant <yashvantyadav855@gmail.com>
2023-11-28 13:48:49 +0530

    implementing DI

--- app/src/main/java/com/example/codev/di/AppModule.kt
+++ app/src/main/java/com/example/codev/di/AppModule.kt
@@ -1,2 +1,24 @@
 package com.example.codev.di
 
+import com.example.codev.utils.Constants.NAME_PROPERTY
+import com.example.codev.utils.Constants.POSTS_COLLECTION
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule{
+
+    @Provides
+    @Singleton
+    fun provideQueryPostsByName() = FirebaseFirestore.getInstance()
+        .collection(POSTS_COLLECTION)
+        .orderBy(NAME_PROPERTY, Query.Direction.ASCENDING)
+
+}
---
+++ app/src/main/java/com/example/codev/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.codev.utils
+
+object Constants {
+    const val POSTS_COLLECTION = "users"
+    const val NAME_PROPERTY = "name"
+}
