81e418a79d1ced09e122ff741a2b56114e97d8fa
YadavYashvant <yashvantyadav855@gmail.com>
2024-05-15 23:50:51 +0530

    Adding sign in animation

---
+++ app/src/main/java/com/example/codev/animations/LoginPageAnim.kt
@@ -0,0 +1,32 @@
+package com.example.codev.animations
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.codev.R
+
+@Composable
+fun AnimatedPreloaderLogin(modifier: Modifier = Modifier) {
+    val preloaderLottieComposition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(
+            R.raw.sign_in
+        )
+    )
+
+    val preloaderProgress by animateLottieCompositionAsState(
+        preloaderLottieComposition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
+
+    LottieAnimation(
+        composition = preloaderLottieComposition,
+        progress = preloaderProgress,
+        modifier = modifier
+    )
+}
