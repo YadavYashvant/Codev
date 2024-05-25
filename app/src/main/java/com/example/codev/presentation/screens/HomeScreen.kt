e0b157c951af314c7a94caa8df646772555f9d06
YadavYashvant <yashvantyadav855@gmail.com>
2024-05-25 23:32:25 +0530

    Improving card scrollview

--- app/src/main/java/com/example/codev/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/codev/presentation/screens/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.example.codev.presentation.screens
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -18,6 +19,7 @@ import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -58,7 +60,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -66,6 +75,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.example.codev.R
 import com.example.codev.animations.AnimatedPreloaderBill
 import com.example.codev.components.CircularProgressBar
 import com.example.codev.components.PostCard
@@ -78,9 +88,14 @@ import com.example.codev.presentation.FilterChipHome
 import com.example.codev.presentation.ProfileDialog
 import com.example.codev.presentation.sign_in.GoogleAuthUiClient
 import com.example.codev.presentation.sign_in.UserData
+import com.example.codev.ui.AppBarCollapsedHeight
+import com.example.codev.ui.AppBarExpendedHeight
 import com.example.codev.ui.theme.greenV
+import com.google.accompanist.insets.LocalWindowInsets
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
+import kotlin.math.max
+import kotlin.math.min
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalCoroutinesApi::class)
@@ -112,6 +127,19 @@ fun HomeScreen(
             drawerState.close()
         }
     }
+
+    val scrollState = rememberLazyListState()
+
+    val imageHeight = AppBarExpendedHeight - AppBarCollapsedHeight
+
+    val maxOffset =
+        with(LocalDensity.current) { imageHeight.roundToPx() } - LocalWindowInsets.current.systemBars.layoutInsets.top
+
+    val offset = min(scrollState.firstVisibleItemScrollOffset, maxOffset)
+
+    val offsetProgress = max(0f, offset * 3f - 2f * maxOffset) / maxOffset
+
+
     ModalNavigationDrawer(
         drawerState = drawerState,
         modifier = Modifier.background(MaterialTheme.colorScheme.onPrimary),
@@ -191,61 +219,93 @@ fun HomeScreen(
                 ) {
                     Spacer(modifier = Modifier.height(50.dp))
 
+//                    val offsetProgress = max(0f, 1f * 3f - 2f * 20f) / 20f
+
                     posts?.let {
                         LazyColumn {
                             item {
 
-                                AnimatedPreloaderBill(
+                                Box(
                                     modifier = Modifier
-                                        .height(300.dp)
-
-                                        .fillMaxWidth()
-                                    //.padding(top = 40.dp)
-                                )
-
-                                TextField(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(horizontal = 12.dp)
-
-                                    ,
-                                    shape = MaterialTheme.shapes.extraLarge,
-                                    colors = TextFieldDefaults.textFieldColors(
-                                        unfocusedIndicatorColor = Color.Transparent,
-                                        focusedIndicatorColor = Color.Transparent
-                                    ),
-                                    value = state.searchText,
-                                    onValueChange = {newText ->
-                                        close_search.value = true
-                                        postsviewModel.onAction(
-                                            UserAction.TextFieldInput(newText)
-                                        )
-                                    },
-                                    placeholder = {
-                                        Text(
-                                            text = "Search prjects with tech stack ...",
-                                        )
-                                    },
-                                    leadingIcon = {
-                                        Icon(
-                                            imageVector = Icons.Filled.Search,
-                                            contentDescription = "Search Icon",
-                                        )
-                                    },
-                                    trailingIcon = {
-                                        if(close_search.value){
-                                            IconButton(onClick = {
-                                                postsviewModel.onAction(UserAction.TextFieldInput(""))
-                                                close_search.value = false
-                                            }) {
-                                                Icon(
-                                                    imageVector = Icons.Filled.Close,
-                                                    contentDescription = "Close Icon",
-                                                )
-                                            }
+                                        .wrapContentHeight()
+                                        .graphicsLayer {
+                                            alpha = 1f - offsetProgress
                                         }
-                                    },
-                                )
+                                ) {
+
+                                    AnimatedPreloaderBill(
+                                        modifier = Modifier
+                                            .height(300.dp)
+
+                                            .fillMaxWidth()
+                                        //.padding(top = 40.dp)
+                                    )
+
+                                    /*Image(
+                                        painter = painterResource(id = R.drawable.cafe),
+                                        contentScale = ContentScale.Crop,
+                                        modifier = Modifier.height(400.dp),
+                                        contentDescription = "cafe"
+                                    )*/
+
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .background(
+                                                Brush.verticalGradient(
+                                                    colorStops = arrayOf(
+                                                        Pair(0.4f, Transparent),
+                                                        Pair(1f, White)
+                                                    )
+                                                )
+                                            )
+                                    )
+
+                                }
+
+                                    TextField(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(horizontal = 12.dp)
+
+                                        ,
+                                        shape = MaterialTheme.shapes.extraLarge,
+                                        colors = TextFieldDefaults.textFieldColors(
+                                            unfocusedIndicatorColor = Color.Transparent,
+                                            focusedIndicatorColor = Color.Transparent
+                                        ),
+                                        value = state.searchText,
+                                        onValueChange = {newText ->
+                                            close_search.value = true
+                                            postsviewModel.onAction(
+                                                UserAction.TextFieldInput(newText)
+                                            )
+                                        },
+                                        placeholder = {
+                                            Text(
+                                                text = "Search prjects with tech stack ...",
+                                            )
+                                        },
+                                        leadingIcon = {
+                                            Icon(
+                                                imageVector = Icons.Filled.Search,
+                                                contentDescription = "Search Icon",
+                                            )
+                                        },
+                                        trailingIcon = {
+                                            if(close_search.value){
+                                                IconButton(onClick = {
+                                                    postsviewModel.onAction(UserAction.TextFieldInput(""))
+                                                    close_search.value = false
+                                                }) {
+                                                    Icon(
+                                                        imageVector = Icons.Filled.Close,
+                                                        contentDescription = "Close Icon",
+                                                    )
+                                                }
+                                            }
+                                        },
+                                    )
 
                                 Spacer(modifier = Modifier.height(20.dp))
 
@@ -281,7 +341,9 @@ fun HomeScreen(
                                     Spacer(modifier = Modifier.height(8.dp))
                                     PostCard(post = post)
                                     Spacer(modifier = Modifier.height(8.dp))
-                                    Divider(modifier = Modifier.height(1.dp).padding(horizontal = 12.dp))
+                                    Divider(modifier = Modifier
+                                        .height(1.dp)
+                                        .padding(horizontal = 12.dp))
 //                                    Divider(modifier = Modifier.padding(vertical = 0.5.dp, horizontal = 12.dp))
 
                                 }
@@ -292,7 +354,9 @@ fun HomeScreen(
                                     Spacer(modifier = Modifier.height(8.dp))
                                     PostCard(post = post)
                                     Spacer(modifier = Modifier.height(8.dp))
-                                    Divider(modifier = Modifier.height(1.dp).padding(horizontal = 12.dp))
+                                    Divider(modifier = Modifier
+                                        .height(1.dp)
+                                        .padding(horizontal = 12.dp))
 //                                    Divider(modifier = Modifier.padding(vertical = 0.5.dp, horizontal = 12.dp))
                                 }
                             }
@@ -327,7 +391,7 @@ fun HomeScreen(
                 ) {
                     Icon(
                         imageVector = Icons.Filled.Add, contentDescription = "add project",
-                        tint = Color.Black
+//                        tint = Color.Black
                     )
                 }
             }
---
+++ app/src/main/java/com/example/codev/ui/Consts.kt
@@ -0,0 +1,6 @@
+package com.example.codev.ui
+
+import androidx.compose.ui.unit.dp
+
+val AppBarCollapsedHeight = 56.dp
+val AppBarExpendedHeight = 400.dp
