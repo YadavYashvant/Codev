a1e1ddce6dd630d5549e80ba49345d3786dc8586
YadavYashvant <yashvantyadav855@gmail.com>
2024-05-28 23:54:45 +0530

    updating cards

--- app/src/main/java/com/example/codev/components/PostCard.kt
+++ app/src/main/java/com/example/codev/components/PostCard.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -140,6 +141,11 @@ fun PostCard(
             }
 
             Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
             IconButton(onClick = {
                 //post.isSaved = true
                 addtosavedcollections(
@@ -164,6 +170,13 @@ fun PostCard(
                     )
                 }
             }
+
+                IconButton(onClick = { /*TODO*/ }
+                ) {
+                    Icon(painter = painterResource(id = R.drawable.chat), contentDescription = "Inquire")
+                    Text(text = "Inquire")
+                }
         }
+            }
     }
 }
